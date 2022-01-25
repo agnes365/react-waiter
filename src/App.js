e26e7cce0d6ba1057c7199f22641ac60f6d729f7
Agata Jarmoszuk <agnes365@vp.pl>
2022-01-25 01:21:19 +0100

    Change spinner

--- src/App.js
+++ src/App.js
@@ -2,25 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home'
 import TableEdit from './components/pages/TableEdit/TableEdit'
 import NotFound from './components/pages/NotFound/NotFound'
-import { Container, Spinner } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Footer from './components/views/Footer/Footer';
 import Header from './components/views/Header/Header';
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { fetchTables } from "./redux/tablesRedux";
 
 function App() {
-
-  const dispatch = useDispatch();
-  const [loader, setLoader] = useState(true);
-  useEffect(() => dispatch(fetchTables(() => setLoader(false))), [dispatch]);
-
   return (
-    loader ?
-      <Container className="d-flex justify-content-center">
-        <Spinner animation="border" />
-      </Container>
-      :
       <Container>
         <Header />
         <Routes>
--- src/components/features/Tables/Tables.js
+++ src/components/features/Tables/Tables.js
@@ -1,18 +1,27 @@
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { getAllTables } from "../../../redux/tablesRedux";
+import { getAllTables, fetchTables } from "../../../redux/tablesRedux";
 import Table from '../../views/Table/Table';
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 
 const Tables = () => {
 
+    const dispatch = useDispatch();
+    const [loader, setLoader] = useState(true);
+    useEffect(() => dispatch(fetchTables(() => setLoader(false))), [dispatch]);
+    
     const tables = useSelector(getAllTables);
 
     return (
-        <ListGroup variant="flush">
-            {tables.map(table => (
-                <Table key={table.id} id={table.id} status={table.status} bill={table.bill} />
-            ))}
-        </ListGroup>
+        loader ?
+            <Spinner animation="border" />
+            :
+            <ListGroup variant="flush">
+                {tables.map(table => (
+                    <Table key={table.id} id={table.id} status={table.status} bill={table.bill} />
+                ))}
+            </ListGroup>
     );
 }
 
